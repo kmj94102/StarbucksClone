8d3ffc008abf788ce51fb175dff831df958caca3
김민재 <kmj94102@naver.com>
2022-11-29 16:14:45 +0900

    * 다시 작업을 하기 위한 프로젝트 초기화

---
+++ app/src/main/java/com/example/starbucksclone/view/main/order/OrderSceen.kt
@@ -0,0 +1,9 @@
+package com.example.starbucksclone.view.main.order
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun OrderScreen() {
+    Text(text = "Order Screen")
+}
