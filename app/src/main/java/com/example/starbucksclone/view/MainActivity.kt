e3fc1e90ab58b1d2a63d47c11b3b6e4e1baeed1d
김민재 <kmj94102@naver.com>
2022-11-29 16:36:17 +0900

    * 다시 작업을 하기 위한 프로젝트 초기화

--- app/src/main/java/com/example/starbucksclone/view/MainActivity.kt
+++ app/src/main/java/com/example/starbucksclone/view/MainActivity.kt
@@ -3,22 +3,13 @@ package com.example.starbucksclone.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.starbucksclone.R
 import com.example.starbucksclone.ui.theme.StarbucksCloneTheme
-import com.example.starbucksclone.view.common.MainTitle
 import com.example.starbucksclone.view.navigation.NavigationGraph
 import dagger.hilt.android.AndroidEntryPoint
 
